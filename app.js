4cb81f28099c2fe0f2f479b9af3a067481110fc1
ShrutiPhatak25 <shrutiphatak222@gmail.com>
2017-04-22 11:24:37 +0530

    added mongolab

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/easyDonations', function(err){
     // not getting printed on console
-mongoose.connect('process.env.MONGOLAB_URI', function(err){
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/easyDonations'  , function(err){
     if(err){
         console.log("Connection refused");
         throw err;
